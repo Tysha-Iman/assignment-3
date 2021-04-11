ef2e16954bdab7e18e4d39fbbdf7ad918895523e
Latysha Mohamed <onepiecemeatyourface@hotmail.com>
2021-04-11 17:37:40 -0600

    Graphics Done... i guess

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
 var vidcapture, ctracker, drawcanvas;
+var outerDiam = 0;
 
 function setup() {
 	var cnv = createCanvas(windowWidth/2, windowHeight/2);
@@ -7,7 +8,7 @@ function setup() {
 
 	// p5 method for creating a video stream
 	vidcapture = createCapture(VIDEO);
-	vidcapture.size(vidcapture.width*2, vidcapture.height*3);
+	vidcapture.size(vidcapture.width/2, vidcapture.height/2);
 	vidcapture.hide()
 
 	//start the tracker
@@ -21,29 +22,34 @@ function setup() {
 
 function draw() {
 
-	background(0, 50)
+	background(0, 10)
 	translate(vidcapture.width, 0)
 	scale(-1, 1)
 
-	// image(vidcapture, 0, 0);
-
 	var position = ctracker.getCurrentPosition();
 
-	
 	if (position) {
+		// ctracker.draw(drawcanvas)
 		position.forEach(function(pos) {
-
-  		var r = map(position[62][0], 210, 480, 0, 255, true);
-  		var b = map(position[62][1], 98, 200, 0, 255, true);
-	
-		noStroke()
-		fill(r, 0, b)
-		ellipse(pos[0],pos[1], 9);
-
-		var xRotate = map(mouseX, 0, width, 0, 20)
+  			var r = map(position[62][0], 0,  width, 0, 255, true);
+  			var b = map(position[62][1], 0, height, 0, 255, true);
+				noStroke()
+				fill(r, 0, b)
+				ellipse(pos[0],pos[1], 9);
+		var xRotate = map(mouseX, 0, width, 0, 90)
 		angleMode(DEGREES)
 		rotate(xRotate);
-		// line(0, 0, 50, 50)
-		})
+  		})
+  			//Ripple effect - for reals slow in browser	
+		for (var i = 0; i < 5; i++){
+			var diam = outerDiam - 30 * i; 
+				if (diam > 0){
+		      		var fade = map(diam, 0, width, 0, 255, true);
+					stroke(fade);
+		      		noFill();
+		     		 ellipse(mouseX, mouseY, diam);
+   					 }
+  			}
+  		outerDiam = outerDiam + 5;
 	}
 }
